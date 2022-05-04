09d0d90fd072ba8ce2242e9c8b04f9a08cb69bd6
KeithMurph <keithmurphy94@gmail.com>
2022-05-04 14:49:37 -0700

    basic loops adds

--- loop.py
+++ loop.py
@@ -1,3 +1,11 @@
+def odds():
+    num = random (0, 100)
+    print(num)
+    
+# def a function
+def click():
+    print("ollie")
+
 # count
 
 for i in range(0, 101):
@@ -9,6 +17,11 @@ for i in range(0, 101):
 
 i =  0
 while i < 101:
-    print(i ,' is a number')
+    print(i)
+    click();
+    odds();
     i = i + 1
+
+
+
     
