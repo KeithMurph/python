c270a0494b0924a3c79aae711d76c99cbebbc1b1
KeithMurph <keithmurphy94@gmail.com>
2022-05-03 21:59:44 -0700

    removed range and simplified

--- grader.py
+++ grader.py
@@ -5,40 +5,39 @@ from time import sleep
 
 sleep(1)
 studentName = str.lower(input('Enter your Name '))
-students = ['bob', 'mary', 'joe'] 
+students = ['bob', 'mary', 'joe']
 
 
 
 if studentName in students:
     print('Welcome back ', studentName)
-    
+
 elif studentName not in students :
     print('hmm ', studentName, ' you say') ; sleep(.9)
     print('You must be a new student ') ; sleep (.5)
     print('lets add you to the list ')
-    print(students)    
+    print(students)
     students.append(studentName)
-
-
-
+    print(students)
 sleep(.5)
 
 
 
 
-print(students)
+
 
 if studentName in students:
     print ("Please enter your test score:")
 grade = int(input())
-
-if int(grade) in range(90, 100):
+if int(grade) ==100:
+    print('PERFECT SCORE!')
+elif int(grade) >=90:
     print ("Great Work! 'A'")
-elif int(grade) in range(80, 89):
+elif int(grade)>= 80:
      print ("Good Job! 'B'")
-elif int(grade) in range(70, 79):
+elif int(grade) >=70:
      print ("Almost there. 'C'")
-elif int(grade) in range(60, 69):
+elif int(grade) >=60:
      print ("Needs a lot of work. 'D'")
 else:
      print ("Not even close! 'F'")
