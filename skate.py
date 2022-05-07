e3aff88e0b5dc3b25432253cd5b5a14f5cbc7fc7
KeithMurph <keithmurphy94@gmail.com>
2022-05-06 18:51:41 -0700

    updated again

--- skate.py
+++ skate.py
@@ -6,7 +6,7 @@ trickList = ['ol' , 'kf', 'hf', 'bssh', 'fssh', 'tre']
 # trickList = [" 'list' to view tricks 'ol' to ollie 'kf' to kickflip 'hf' to heelflip 'bssh' backside shuv 'fssh' frontside shuv 'tre' to treflip 'exit' to exit"]
 odds = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100]
 game_active = True
-health = (10)
+health = (8)
 SKATE = []
 skateFull = ['S', 'K', 'A', 'T', 'E']
 lineBonus = (0)
@@ -55,7 +55,7 @@ while game_active == True:
                 print(SKATE)
                 print('GAME OVER ☠️')
                 game_active = False
-
+            
        
 
 
